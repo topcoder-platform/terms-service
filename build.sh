bf2e1ec6c8f4a9d94a384f33003c3957fe7d66ca
Gunasekar-K <gunasekar.kesavalu@wipro.com>
2019-05-06 11:03:23 +0530

    added cache setting to increase build permormance

--- build.sh
+++ build.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
 set -eo pipefail
+UPDATE_CACHE=""
 echo "" > docker/api.env
 docker-compose -f docker/docker-compose.yml build terms-service
 #docker images
+docker create --name app terms-service:latest
+if [ -d node_modules ]
+then
+  mv package-lock.json old-package-lock.json
+  docker cp app:/terms-service/package-lock.json package-lock.json
+  set +eo pipefail
+  UPDATE_CACHE=$(cmp package-lock.json old-package-lock.json)
+  set -eo pipefail
+else
+  UPDATE_CACHE=1
+fi
+
+if [ "$UPDATE_CACHE" == 1 ]
+then
+  docker cp app:/terms-service/node_modules .
+fi
